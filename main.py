5e4d9378412e53c0ea4074d1c9c9939ca9f017a9
Aashirwad10 <aashbudh10@gmail.com>
2025-09-15 23:33:13 +0545

    Data quality check

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("education_inequality_data.csv")
+
+# Level 1: Understanding & Cleaning the Data
+# A. Data Quality Check
+print(df.head())
+print(df.info())
+print(df.describe())
+print(df.isnull().sum()) # check missing value
+print(df.duplicated().sum()) # check duplicated sum
