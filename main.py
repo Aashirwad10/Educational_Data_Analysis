929c8102dca0a6df24a7efb46c59d5d343df8b49
Aashirwad10 <aashbudh10@gmail.com>
2025-09-15 23:34:58 +0545

    Basic Summaries

--- main.py
+++ main.py
@@ -10,3 +10,14 @@ print(df.info())
 print(df.describe())
 print(df.isnull().sum()) # check missing value
 print(df.duplicated().sum()) # check duplicated sum
+
+# B. Basic Summaries
+# Number of schools by state,school type and grade level
+school_by_state = df['state'].value_counts() # for state
+print(school_by_state)
+
+school_by_type = df['school_type'].value_counts() # for type
+print(school_by_type)
+
+school_by_level = df['grade_level'].value_counts() # for grade level
+print(school_by_level)
